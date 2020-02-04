1077a756c0120387073831a9c18b93c41b6d9bcf
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-04 02:05:10 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,26 +1,111 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+	countries_and_capitals_of_the_world = {
+  :railroads => {
+    }
+	}
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  countries_and_capitals_of_the_world = {
+  :railroads => {
+    :pieces => 4,
+    "USA" => {
+      :capital => "Washington D.C.",
+      :capital_climate => "Kőppen Cfa"
+    }
+  },
+  :africa => {
+    "Ghana" => {
+      :capital => "Accra",
+      :capital_climate => "Kőppen Aw"
+    },
+    "Nigeria" => {
+      :capital => "Abuja",
+      :capital_climate => "Kőppen Aw"
+    }
+  }
+}
+ 
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  countries_and_capitals_of_the_world = {
+  :railroads => {
+    :pieces => 4,
+    :rent_in_dollars =>  {
+      :four_pieces_owned => 200,
+      :three_pieces_owned => 100,
+      :two_pieces_owned => 50,
+      :one_piece_owned => 25
+      
+    },
+    :names => {
+      :reading_railroad => {
+        
+      },
+      :pennsylvania_railroad => {
+        
+      },
+      :b_and_o_railroad => {
+        
+      },
+      :shortline_railroad => {
+        
+      },
+    }
+  },
+  :africa => {
+    "Ghana" => {
+      :capital => "Accra",
+      :capital_climate => "Kőppen Aw"
+    },
+    "Nigeria" => {
+      :capital => "Abuja",
+      :capital_climate => "Kőppen Aw"
+    }
+  }
+}
+ 
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  countries_and_capitals_of_the_world = {
+  :railroads => {
+    :pieces => 4,
+    :rent_in_dollars =>  {
+      :four_pieces_owned => 200,
+      :three_pieces_owned => 100,
+      :two_pieces_owned => 50,
+      :one_piece_owned => 25
+      
+    },
+    :names => {
+      :reading_railroad => {
+        "mortgage_value" => "$100"
+      },
+      :pennsylvania_railroad => {
+        'mortgage_value' => '$200'
+      },
+      :b_and_o_railroad => {
+        'mortgage_value' => '$400'
+      },
+      :shortline_railroad => {
+        'mortgage_value' => '$800'
+      },
+    }
+  },
+  :africa => {
+    "Ghana" => {
+      :capital => "Accra",
+      :capital_climate => "Kőppen Aw"
+    },
+    "Nigeria" => {
+      :capital => "Abuja",
+      :capital_climate => "Kőppen Aw"
+    }
+  }
+}
+ 
 end
